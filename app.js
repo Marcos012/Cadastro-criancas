f028fd918c700e596244d71e2404db2ffc980803
marcos012 <marcospaulo.ad@gmail.com>
2018-09-11 10:00:26 -0300

    Adicina tela e rota de cadastro + ejs

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+const port = process.env.PORT || 3000
 
-app.listen(3000);
+
+app
+    .set('views', __dirname + '/views')
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => {
+        res.render('index')
+    })
+    .get('/cadastro', (req, res) => {
+        res.render('cadastro')
+    })
+
+
+
+app.listen(port, () => console.log('Servidor rodando em http://localhost:'+port));
