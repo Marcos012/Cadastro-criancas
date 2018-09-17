6035eafc0168d47ef0192f3401f6f3d187e55a00
marcos012 <marcospaulo.ad@gmail.com>
2018-09-16 23:47:01 -0300

    faz modificações no layout e arruma bug de rota

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
-const port = process.env.PORT || 3000
-
-
 app
-    .set('views', __dirname + '/views')
     .set('view engine', 'ejs')
+    .set('views', __dirname + '/views')
+    .use(expressLayouts)    
+    .use('/public', express.static(__dirname + '/public/'))
+    .use(cookieParser())
+    .use(bodyParser.urlencoded({ extended: false }))
     .get('/', (req, res) => {
-        res.render('index')
+        res.render('pages/home')
     })
     .get('/cadastro', (req, res) => {
-        res.render('cadastro')
+        res.render('pages/cadastro')
     })
-
-
-
-app.listen(port, () => console.log('Servidor rodando em http://localhost:'+port));
+    .listen(PORT, () => console.log('Servidor rodando em http://localhost:' + PORT));
